bf3cf817f47b07a376b5394993dab9ae008e14ad
Mandeep Singh <mandeepsingh@mandeeps-air.lan>
2024-01-10 23:56:08 -0800

    Added some code to JS file.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,32 @@
-// Assignment code here
+var passwordText = document.querySelector("#password");
 
+function generatePassword() {
+  
+  var passwordLength = prompt("Choose a password length (between 8 and 128 characters):");
+  
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Invalid password length. Please enter a number between 8 and 128.");
+    return;
+  }
+
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("At least one character type must be selected.");
+    return;
+  }
+
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()-_=+[]{};:,.<>/?"
+  
+  var characterPool = "";
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
